56d1c1c9970e3fa30c7c88e9990142b7674b1601
ice036 <ice036@qq.com>
2023-03-31 01:27:37 +0800

    update website title

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   // 打包项目时，使用相对路径
-  publicPath: './'
+  publicPath: './',
+  // 修改网站的标题
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '起始导航页'
+      return args
+    })
+  }
 })
